1b58cad66a8056ff061c7e7ba2611beabf0a4e1b
p3dru <61830482+p3dru@users.noreply.github.com>
2021-11-28 12:10:17 -0300

    Delete 2020.1/atividade 2 livro directory

--- 2020.1/atividade 2 livro/cap_1/pense_em_python_cap_1_q1.2.1.py
+++
@@ -1,4 +0,0 @@
-minutos = 42
-segundos = 42
-segundos_totais = (minutos * 60) + segundos
-print(segundos_totais)
--- 2020.1/atividade 2 livro/cap_2/pense_em_python_cap_2_q2.2.1.py
+++
@@ -1,5 +0,0 @@
-#resolução única
-raio = 5
-pi = 3.14
-volume_esfera = raio ** 3 * 3.14 * 3 / 4
-print(f"Uma esfera com raio {raio} tem volume {volume_esfera:.2f}")
